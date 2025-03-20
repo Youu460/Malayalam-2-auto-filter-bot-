1691d2ffa7fc12c64c111a51829e151b711c2730
MMW BOTZ <155316405+mmwbotzmain@users.noreply.github.com>
2025-03-20 21:02:10 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -111,7 +111,7 @@ Total Members = <code>{}</code>
 Added By - {}
 """
     RESULT_TXT="""<blockquote> Hey,     </blockquote>
-<pre> ᴍʏ ɴᴀᴍᴇ ɪꜱ Eʟꜱᴀ </pre>
+
 <blockquote>Jᴜsᴛ Sᴇᴇ Wʜᴀᴛ I Found Fᴏʀ Yᴏᴜʀ Qᴜᴇʀʏ {} </blockquote>"""
 
     CUSTOM_FILE_CAPTION = """<pre>📂Fɪʟᴇɴᴀᴍᴇ : {file_name}
